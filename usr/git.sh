a110a87bfb3348f31f72f7d1c1644f9fd348a2b1
ayakashi <gwanmax@gmail.com>
2016-04-19 11:52:19 +0800

    Add some git related scripts

---
+++ usr/git.sh
@@ -0,0 +1,19 @@
+# update all repository under the direcotry
+for d in `ls`
+do
+    if [[ -d $d ]]; then
+        cd $d
+        git pull origin master
+        cd ..
+    fi
+done
+
+# who and when push the first commit
+for d in `ls`
+do
+    if [[ -d $d ]]; then
+        cd $d
+        echo $d: $(git log | tail -n 6 | grep -E "Date|Author")
+        cd ..
+    fi
+done
