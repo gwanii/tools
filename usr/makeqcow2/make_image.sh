0a83f3bcc21fe6905dcd1b7ca4fefd0c3908f2bd
ayakashi <gwanmax@gmail.com>
2016-11-25 12:19:57 +0800

    oz-install make qcow2 image

---
+++ usr/makeqcow2/make_image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# yum install -y oz createrepo pykickstart
+# vim /etc/oz/oz.cfg ==> "image_type=qcow2"
+# reboot??
+# createrepo /root/img/files/repo(the dir include some rpms)
+# kill -9 $(netstat -ntlp|grep 8000|cut -d/ -f1|awk '{print $7}')
+# cd /root/img/files && sh -c "nohup python -m SimpleHTTPServer > /dev/null &" && cd ..
+oz-install -p -u -d3 -a ks.cfg centos70.tdl -t 12000
+qemu-img convert -c /var/lib/libvirt/images/centos_70_x86_64.qcow2 -O qcow2 ~/epc.qcow2
+
+# yum install -y guestfish
+# export LIBGUESTFS_BACKEND=direct
+# guestfish --rw -a epc.qcow2
+# run
+# list-filesystems
+# mount /dev/sda3 /
+# mkdir /opt/openstack
+# vi /opt/openstack/policy_routing.sh
