c01211d80bbfce017a6355426d850b62bd803d7d
ayakashi <gwanmax@gmail.com>
2018-07-03 10:29:59 +0800

    sudo no passwd

---
+++ usr/sudo_no_passwd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+USER=ubuntu
+
+sudo_no_passwd() {
+  grep -q "^$USER:" /etc/passwd || sudo adduser "$USER"
+  sudo usermod -a -G sudo "$USER" && \
+  echo '$USER ALL=(ALL) NOPASSWD:ALL' | sudo tee --append /etc/sudoers.d/90-cloud-init-users
+}
+
+sudo_no_passwd
